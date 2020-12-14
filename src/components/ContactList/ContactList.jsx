14d2726f1eb0f7ba48173fe79bdb0b687711fe62
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-12-14 14:56:34 +0200

    Убрала рендер по условию

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+import list from '../../transitions/list.module.css';
+import styles from './ContactList.module.css';
+
+
+const ContactList = ({ items, onDelete }) => {
+  return   (
+    <TransitionGroup component="ul" className={styles.ContactList}>
+        {items.map(({ name, id, number }) => {
+          return (
+            <CSSTransition 
+            key={id} 
+            timeout={250} 
+            classNames={list}
+            unmountOnExit>
+            <li>
+              <span>
+                {name} {number}
+              </span>
+              <button
+                type="button"
+                onClick={() => onDelete(id)}
+              >
+                Delete
+              </button>
+            </li>
+            </CSSTransition>
+          );
+        })}
+      </TransitionGroup>
+    );
+    // );
+      };
+
+ContactList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+export default ContactList;
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Notification.module.css';
+
+const Notification = () => {
+  return <div className={styles.notification}>Contact already exist!</div>;
+};
+
+export default Notification;
